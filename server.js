0e51fc6e8747a125e65f33c5128a88346857e659
Fabricio <fabricio.bizotto@ifc.edu.br>
2024-10-14 23:03:44 -0300

    host

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = require('./src/app');
 
+const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} - ${NODE_ENV}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT} in ${NODE_ENV} mode`);
 });
