fba92c480a91480caa2254eb6cdb5bbcf09e753f
Fabricio <fabricio.bizotto@ifc.edu.br>
2024-10-14 22:31:56 -0300

    commit

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} - ${NODE_ENV}`);
 });
